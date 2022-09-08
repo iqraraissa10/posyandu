9dffc5c80d37ded0d91a508630b505be57b2c7b2
iqraraissa10 <90818904+iqraraissa10@users.noreply.github.com>
2022-09-08 14:02:53 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<div class="well">
+			<h3>Penjelasan Singkat</h3>
+			<hr/>
+			Aplikasi ini adalah aplikasi sistem pendukung keputusan untuk menentukan nilai gizi pada balita dengan metode saw (simple additive weighting) yang mengharuskan
+			untuk menginput nilai dulu kemudian baru kriteria dan selanjutnya adalah alternatif dan tahap akhir dengan melakukan
+			perangkingan pada rangking dan melihat laporan hasil akhir.<p>&nbsp;</p>
+		</div>
+	</div>
+	
