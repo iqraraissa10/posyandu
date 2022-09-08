a0fe3b0988f3dbf4edfcccc3a550cc9a10a54236
iqraraissa10 <90818904+iqraraissa10@users.noreply.github.com>
2022-09-08 14:02:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+include_once 'header.php';
+include_once 'includes/nilai.inc.php';
+$pro3 = new Nilai($db);
+$stmt3 = $pro3->readAll();
+include_once 'includes/alternatif.inc.php';
+$pro1 = new Alternatif($db);
+$stmt1 = $pro1->readAll();
+$stmt4 = $pro1->readAll();
+include_once 'includes/kriteria.inc.php';
+$pro2 = new Kriteria($db);
+$stmt2 = $pro2->readAll();
+?>
+		
+		<div class="well"><div id="container2" style="min-width: 310px; height: 400px; margin: 0 auto"></div></div>
+	
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1.11.3.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/highcharts.js"></script>
+	<script src="js/exporting.js"></script>
+	<script>
+	var chart1; // globally available
+	$(document).ready(function() {
+	      chart1 = new Highcharts.Chart({
+	         chart: {
+	            renderTo: 'container2',
+	            type: 'column'
+	         },  
+	         title: {
+	            text: 'Grafik Perangkingan '
+	         },
+	         xAxis: {
+	            categories: ['Alternatif']
+	         },
+	         yAxis: {
+	            title: {
+	               text: 'Jumlah Nilai'
+	            }
+	         },
+	              series:            
+	            [
+	            <?php
+	            while ($row4 = $stmt4->fetch(PDO::FETCH_ASSOC)){
+	                  ?>
+	                 //data yang diambil dari database dimasukan ke variable nama dan data
+	                 //
+	                  {
+	                      name: '<?php echo $row4['nama_alternatif'] ?>',
+	                      data: [<?php echo $row4['hasil_alternatif'] ?>]
+	                  },
+	                  <?php } ?>
+	            ]
+	      });
+	   });  
+	   </script>
+	</body>
+</html>
